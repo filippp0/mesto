5c4692b5dd1830c520157dc5c58e980ada08d7ab
Filipp Kolesnikov <filippp0@yandex.ru>
2023-02-26 19:06:28 +0300

    build(body): сделать popup и переписать отступы под макет

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
-const editButton = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
-const closePopup = document.querySelector('.popup__close-icon');
+let editButton = document.querySelector(".profile__edit");
 
-let form = document.querySelector('.popup__container');
-let nameUser = document.querySelector('.profile__name');
-let jobUser = document.querySelector('.profile__job');
-let nameInput = document.querySelector('.popup__user-name');
-let jobInput = document.querySelector('.popup__user-job');
+let popup = document.querySelector(".popup");
+
+let closePopup = document.querySelector(".popup__close-icon");
+
+let form = document.querySelector(".popup__container");
+
+let nameUser = document.querySelector(".profile__name");
+
+let jobUser = document.querySelector(".profile__job");
+
+let nameInput = document.querySelector(".popup__user-name");
+
+let jobInput = document.querySelector(".popup__user-job");
 
 nameInput.value = nameUser.textContent;
+
 jobInput.value = jobUser.textContent;
 
-function openPopupEdit() {
-  popup.classList.add('popup_opened');
-}
+editButton.addEventListener("click", () => popup.classList.add("popup_opened"));
 
-editButton.addEventListener('click', openPopupEdit);
+closePopup.addEventListener("click", () => popup.classList.remove("popup_opened"));
 
-function closePopupEdit() {
-  popup.classList.remove('popup_opened');
-}
-
-closePopup.addEventListener('click', closePopupEdit);
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  nameUser.textContent = nameInput.value;
-  jobUser.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
-}
-
-form.addEventListener('submit', handleFormSubmit);
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    nameUser.textContent = nameInput.value;
+    jobUser.textContent = jobInput.value;
+    popup.classList.remove("popup_opened");
+});
